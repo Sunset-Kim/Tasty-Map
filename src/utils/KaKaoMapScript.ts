b6f27d686ee8382fd22d9715af4b838f9b5c6de5
Sunset-kim <wood2002@naver.com>
2022-05-21 17:32:55 +0900

    add :: map 컨트롤러 추가

--- src/utils/KaKaoMapScript.ts
+++ src/utils/KaKaoMapScript.ts
@@ -3,10 +3,16 @@ const kakao = window.kakao;
 
 export default function KaKaoMapScript() {
   const container = document.getElementById('map');
+  const 태양빌딩 = [37.492919, 127.012372];
   const options = {
-    center: new kakao.maps.LatLng(33.450701, 126.570667),
+    center: new kakao.maps.LatLng(태양빌딩[0], 태양빌딩[1]),
     level: 3,
   };
 
+  const mapTypeControl = new kakao.maps.MapTypeControl();
+  const zoomControl = new kakao.maps.ZoomControl();
+
   const map = new kakao.maps.Map(container, options);
+  map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+  map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 }

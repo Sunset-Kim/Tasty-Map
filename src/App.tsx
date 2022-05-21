a2ed521984785dea372fa7a3a3eda6351088e78f
Sunset-kim <wood2002@naver.com>
2022-05-21 15:36:38 +0900

    setup :: tailwind / post css  import + nesting plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   // console.log(import.meta.env.VITE_API_KEY);
 
   return (
-    <div className="container bg-slate-100 m-auto min-h-screen">맛집지도</div>
+    <div className="container bg-slate-100 m-auto min-h-screen title">
+      <h1>서초구 맛집 지도</h1>
+      <button className="btn">버튼</button>
+    </div>
   );
 }
 
